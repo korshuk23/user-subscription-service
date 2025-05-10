94669738edc376d92a6fc9eabf8f8ceca53a2f50
Andrei Korshuk <a.v.korshuk@gmail.com>
2025-05-10 03:50:20 +0300

    delete unused imports

--- src/main/java/com/andrei/dev/subscription_service/dto/UserResponseDto.java
+++ src/main/java/com/andrei/dev/subscription_service/dto/UserResponseDto.java
@@ -1,10 +1,6 @@
 package com.andrei.dev.subscription_service.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
